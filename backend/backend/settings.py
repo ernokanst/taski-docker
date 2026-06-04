c7ea5672d7eafd9997f676e86fba4ee3c7f968ce
ernokanst <ernokanst@gmail.com>
2026-06-04 16:19:34 +0300

    Новый хост

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '158.160.246.194', 'taski.dinkani.com']
+ALLOWED_HOSTS = ['localhost', '147.182.172.0', 'taski.dinkani.com']
 
 
 # Application definition
